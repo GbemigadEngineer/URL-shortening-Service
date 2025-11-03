31265aed2e562f5d2a335b4f50f1c3577b85b3ec
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-11-03 15:04:12 +0100

    made backend changes

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -3,11 +3,13 @@ const {
   createShortUrl,
   getOriginalUrl,
   getAllUrls,
+  redirectToOriginalUrl,
 } = require("../controller/urlController");
 
 const router = express.Router();
 
 router.route("/").post(createShortUrl);
+router.get("/:shortUrl", redirectToOriginalUrl);
 router.route("/lookup/:shortUrl").get(getOriginalUrl);
 router.get("/all", getAllUrls);
 
